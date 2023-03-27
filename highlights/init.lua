f361dbec3e7d7d5d684d8c9a0cb0bf54d3ac27c8
Isak <isak.jones@january.com>
2023-03-27 17:01:00 -0400

    checkpoint

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,5 @@
-return { -- this table overrides highlights in all themes
-  -- Normal = { bg = "#000000" },
+return {
+    -- this table overrides highlights in all themes
+    -- Normal = { bg = "#000000" },
+    Normal = {fg = "#abb2bf", bg = "#010101"}
 }
--- mappings.lua
+++ mappings.lua
@@ -3,26 +3,80 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
-return {
-  -- first key is the mode
+local mappings = {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
-      end,
-      desc = "Pick to close",
+    ["L"] = { "<cmd>bnext<cr>" },
+    ["H"] = { "<cmd>bprev<cr>" },
+    ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bc"] = {
+      "<cmd>BufferLinePickClose<cr>",
+      desc = "Pick to close"
     },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
+    ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
+    ["<leader>bt"] = {
+      "<cmd>BufferLineSortByTabs<cr>",
+      desc = "Sort by tabs"
+    },
+    ["<C-\\>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" }
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
+    ["<esc>"] = { "<C-\\><C-n>", desc = "Terminal normal mode" },
+    ["<C-\\>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" }
+  }
 }
+
+if os.getenv("COLEMAK") == "true" then
+  mappings["n"]["I"] = { "<cmd>bnext<cr>" }
+  mappings["n"]["M"] = { "<cmd>bprev<cr>" }
+  mappings["n"]["m"] = { "h" }
+  mappings["n"]["n"] = { "j" }
+  mappings["n"]["e"] = { "k" }
+  mappings["n"]["i"] = { "l" }
+  mappings["n"]["N"] = { "J" }
+  mappings["n"]["E"] = { "K" }
+  mappings["n"]["h"] = { "m" }
+  mappings["n"]["j"] = { "n" }
+  mappings["n"]["k"] = { "e" }
+  mappings["n"]["l"] = { "i" }
+  mappings["n"]["J"] = { "N" }
+  mappings["n"]["K"] = { "E" } -- So K is "end"
+  mappings["v"]["m"] = { "h" }
+  mappings["v"]["n"] = { "j" }
+  mappings["v"]["i"] = { "l" }
+  mappings["v"]["e"] = { "k" }
+  mappings["v"]["M"] = { "H" }
+  mappings["v"]["N"] = { "J" }
+  mappings["v"]["I"] = { "L" }
+  mappings["v"]["E"] = { "K" }
+  mappings["v"]["h"] = { "m" }
+  mappings["v"]["j"] = { "n" }
+  mappings["v"]["k"] = { "e" }
+  mappings["v"]["l"] = { "i" }
+  mappings["v"]["H"] = { "M" }
+  mappings["v"]["J"] = { "N" }
+  mappings["v"]["K"] = { "E" }
+  mappings["v"]["L"] = { "I" }
+  mappings["x"]["m"] = { "h" }
+  mappings["x"]["n"] = { "j" }
+  mappings["x"]["i"] = { "l" }
+  mappings["x"]["e"] = { "k" }
+  mappings["x"]["M"] = { "H" }
+  mappings["x"]["N"] = { "J" }
+  mappings["x"]["I"] = { "L" }
+  mappings["x"]["E"] = { "K" }
+  mappings["x"]["h"] = { "m" }
+  mappings["x"]["j"] = { "n" }
+  mappings["x"]["k"] = { "e" }
+  mappings["x"]["l"] = { "i" }
+  mappings["x"]["H"] = { "M" }
+  mappings["x"]["J"] = { "N" }
+  mappings["x"]["K"] = { "E" }
+  mappings["x"]["L"] = { "I" }
+end
+
+
+return mappings
