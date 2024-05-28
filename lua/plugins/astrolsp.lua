4a3cd989e442013e35adf238352d7ebabb4fa9d8
Isak <isak@alza.app>
2024-05-28 11:28:42 -0400

    checkpoint

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -18,15 +18,15 @@ return {
     -- customize lsp formatting options
     formatting = {
       -- control auto formatting on save
-      format_on_save = {
-        enabled = false, -- enable or disable format on save globally
-        -- allow_filetypes = { -- enable format on save for specified filetypes only
-        --   -- "go",
-        -- },
-        -- ignore_filetypes = { -- disable format on save for specified filetypes
-        --   -- "python",
-        -- },
-      },
+      format_on_save = false,
+      -- enabled = false, -- enable or disable format on save globally
+      -- allow_filetypes = { -- enable format on save for specified filetypes only
+      --   -- "go",
+      -- },
+      -- ignore_filetypes = { -- disable format on save for specified filetypes
+      --   -- "python",
+      -- },
+      -- },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
@@ -38,6 +38,8 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
+      "gleam",
+      "typescript-tools",
       -- "pyright"
     },
     -- customize language server configuration options passed to `lspconfig`
